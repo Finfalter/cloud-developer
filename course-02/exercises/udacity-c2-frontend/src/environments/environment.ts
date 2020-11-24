48a526afd88e6f1ca013ffae0b7adfa77c5bc2f4
Christoph Brewing <finnfalter@gmail.com>
2020-11-24 08:34:25 +0100

    bug fixes

--- course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
+++ course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://uda-brewing-dev-dev.eu-central-1.elasticbeanstalk.com/api/v0'
+  //apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
